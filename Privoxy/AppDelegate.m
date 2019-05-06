c550afff3977780bafd020d7dfe987152cbe3c0f
xuqigang <xuqigang10@163.com>
2019-05-06 17:43:45 +0800

    支持后台运行privoxy

--- Privoxy/AppDelegate.m
+++ Privoxy/AppDelegate.m
@@ -7,8 +7,12 @@
 //
 
 #import "AppDelegate.h"
+#import <CoreLocation/CoreLocation.h>
+@interface AppDelegate ()<CLLocationManagerDelegate>
+{
+    CLLocationManager *appleLocationManager;
+}
 
-@interface AppDelegate ()
 
 @end
 
@@ -17,10 +21,31 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self startLocation];
     return YES;
 }
 
+- (void)startLocation{
+    self->appleLocationManager = [[CLLocationManager alloc] init];
+    self->appleLocationManager.allowsBackgroundLocationUpdates = YES;
+    self->appleLocationManager.pausesLocationUpdatesAutomatically = NO;
+    self->appleLocationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [self->appleLocationManager setDistanceFilter:kCLDistanceFilterNone];
+    self->appleLocationManager.delegate = self;
+    [self->appleLocationManager requestAlwaysAuthorization];
+    [self->appleLocationManager startUpdatingLocation];
+}
+/** 苹果_用户位置更新后，会调用此函数 */
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
+    
+    NSLog(@"success");
+}
 
+/** 苹果_定位失败后，会调用此函数 */
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    
+    NSLog(@"error");
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -30,6 +55,7 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [self->appleLocationManager startUpdatingLocation];
 }
 
 
